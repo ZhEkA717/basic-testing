8b7f7c28e4faf6c3b98e7e35134e27f7d70decda
Zheka <grushevskiy.yevgeniy@mail.ru>
2024-02-11 21:05:31 +0300

    feat: implement mlocking lib api

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,54 @@
-// Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+import { throttledGetDataFromApi } from './index';
+
+type TypeResponce = { id: number; name: string }[];
 
 describe('throttledGetDataFromApi', () => {
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
+  const config = {
+    baseURL: 'https://jsonplaceholder.typicode.com',
+  };
+
+  const relativePath = 'api/users';
+  const responce: TypeResponce = [
+    { id: 1, name: 'user1' },
+    { id: 2, name: 'user2' },
+  ];
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const spyCreate = jest.spyOn(axios, 'create');
+    jest
+      .spyOn(axios.Axios.prototype, 'get')
+      .mockResolvedValueOnce({ data: null });
+    await throttledGetDataFromApi(relativePath);
+    jest.runOnlyPendingTimers();
+    expect(spyCreate).toHaveBeenCalledWith(config);
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const spyGet = jest
+      .spyOn(axios.Axios.prototype, 'get')
+      .mockResolvedValueOnce({ data: null });
+    await throttledGetDataFromApi(relativePath);
+    jest.runOnlyPendingTimers();
+    expect(spyGet).toHaveBeenCalledWith(relativePath);
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    jest
+      .spyOn(axios.Axios.prototype, 'get')
+      .mockResolvedValueOnce({ data: responce });
+    const res: TypeResponce = await throttledGetDataFromApi(relativePath);
+    jest.runOnlyPendingTimers();
+    res.forEach((item, i) => {
+      expect(item.id).toBe(i + 1);
+    });
   });
 });
